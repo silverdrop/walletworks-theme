fb8552850d588cc6e3f65a8719352693b2f5d3e8
silverdrop <yoshitoshi_toyota1991@hotmail.com>
2015-10-27 10:35:46 -0700

    initial commit

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+// enqueue the child theme stylesheet
+
+Function wp_schools_enqueue_scripts() {
+wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  );
+wp_enqueue_style( 'childstyle' );
+}
+add_action( 'wp_enqueue_scripts', 'wp_schools_enqueue_scripts', 11);
