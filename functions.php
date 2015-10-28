3f1e83dcbe4f88238a106fa664f71ce00de9ea7e
silverdrop <yoshitoshi_toyota1991@hotmail.com>
2015-10-28 11:25:16 -0700

    font added

--- functions.php
+++ functions.php
@@ -2,8 +2,33 @@
 
 // enqueue the child theme stylesheet
 
-Function wp_schools_enqueue_scripts() {
-wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  );
-wp_enqueue_style( 'childstyle' );
+function walletworks_enqueue_webfont() {
+	wp_enqueue_style( 'childstyle-font', get_stylesheet_directory_uri() . '/css/webfont.css'  );
 }
-add_action( 'wp_enqueue_scripts', 'wp_schools_enqueue_scripts', 11);
+function walletworks_enqueue_scripts() {
+	wp_enqueue_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css' );
+	wp_enqueue_script( 'childscript', get_stylesheet_directory_uri() . '/js/custom.js', array(), '1.0.0', true );
+}
+add_action( 'wp_enqueue_scripts', 		'walletworks_enqueue_webfont', 10);
+add_action( 'admin_enqueue_scripts', 	'walletworks_enqueue_webfont', 10);
+add_action( 'wp_enqueue_scripts', 		'walletworks_enqueue_scripts', 11);
+
+function walletworks_framework() {
+	global $qodeFramework;
+	$qodeTitle = $qodeFramework->qodeMetaBoxes->getMetaBox("page_title");
+	$qode_page_title_area_container = $qodeTitle->getChild("qode_page_title_area_container");
+	$qode_title_text_container = $qode_page_title_area_container->getChild("qode_title_text_container");
+	
+	$qode_page_custom_title = new QodeMetaField("text","qode_page_custom_title","","Custom Title Text","Enter your custom title text");
+	$qode_title_text_container->addChild("qode_page_custom_title",$qode_page_custom_title);
+}
+add_action( 'admin_init', 'walletworks_framework');
+
+function walletworks_custom_title_text($text) {
+	if(is_page()) {
+		$custom_title = get_post_meta(get_the_ID(), "qode_page_custom_title", true);
+		$text = $custom_title ? $custom_title : $text;
+	}
+	return $text;
+}
+add_filter( 'qode_title_text', 'walletworks_custom_title_text', 10, 1);
