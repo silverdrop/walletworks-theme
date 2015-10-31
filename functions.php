2cd32a5c193fac0fc4ccf5f62565883ea92f50d1
silverdrop <yoshitoshi_toyota1991@hotmail.com>
2015-10-31 04:26:06 -0700

    font updated

--- functions.php
+++ functions.php
@@ -9,8 +9,8 @@ function walletworks_enqueue_scripts() {
 	wp_enqueue_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css' );
 	wp_enqueue_script( 'childscript', get_stylesheet_directory_uri() . '/js/custom.js', array(), '1.0.0', true );
 }
-add_action( 'wp_enqueue_scripts', 		'walletworks_enqueue_webfont', 10);
-add_action( 'admin_enqueue_scripts', 	'walletworks_enqueue_webfont', 10);
+//add_action( 'wp_enqueue_scripts', 		'walletworks_enqueue_webfont', 10);
+//add_action( 'admin_enqueue_scripts', 	'walletworks_enqueue_webfont', 10);
 add_action( 'wp_enqueue_scripts', 		'walletworks_enqueue_scripts', 11);
 
 function walletworks_framework() {
